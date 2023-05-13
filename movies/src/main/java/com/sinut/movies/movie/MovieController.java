48de4262a242428f4556b5a653257c413b7ea16f
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-05-14 00:45:43 +0700

    feat: get movie by id

--- movies/src/main/java/com/sinut/movies/movie/MovieController.java
+++ movies/src/main/java/com/sinut/movies/movie/MovieController.java
@@ -1,12 +1,15 @@
 package com.sinut.movies.movie;
 
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -20,4 +23,8 @@ public class MovieController {
         return ResponseEntity.ok(movieService.allMovies());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable ObjectId id) {
+        return ResponseEntity.ok(movieService.getMovieById(id));
+    }
 }
--- movies/src/main/java/com/sinut/movies/movie/MovieService.java
+++ movies/src/main/java/com/sinut/movies/movie/MovieService.java
@@ -1,9 +1,11 @@
 package com.sinut.movies.movie;
 
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +14,9 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+    
+    public Optional<Movie> getMovieById(ObjectId id) {
+        return movieRepository.findById(id);
     }
 }
