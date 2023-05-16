59cd1e4c8bf7f4785bc8a2924de794eb0ffc75ba
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-05-16 14:15:52 +0700

    feat: modal

--- client/lib/widgets/movie_item_widget.dart
+++ client/lib/widgets/movie_item_widget.dart
@@ -1,5 +1,7 @@
 import 'package:client/models/movie_model.dart';
+import 'package:client/widgets/video_player_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class MovieItemWidget extends StatelessWidget {
   const MovieItemWidget({Key? key, required this.movieModel}) : super(key: key);
@@ -45,7 +47,12 @@ class MovieItemWidget extends StatelessWidget {
           ),
           ElevatedButton(
             onPressed: () {
-              // TODO: Navigate to view frame
+              showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return const VideoPlayerWidget();
+                },
+              );
             },
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.yellow,
