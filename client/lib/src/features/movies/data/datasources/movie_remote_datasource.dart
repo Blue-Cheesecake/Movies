959f2f45e7f049d47222fb3647f86f47d4a9c7c7
Sinut Wattanarporn <s_w.piepie@outlook.com.au>
2023-06-25 20:12:06 +0700

    feat: MV-0 rename

--- client/lib/src/features/movies/data/datasources/movie_remote_datasource.dart
+++ client/lib/src/features/movies/data/datasources/movie_remote_datasource.dart
@@ -11,7 +11,7 @@ abstract class MovieDataSource {
 
 @RestApi(baseUrl: "http://localhost:8080/api/v1/")
 abstract class MovieRetrofit {
-  factory MovieRetrofit(Dio dio) = _MovieRemoteDataSource;
+  factory MovieRetrofit(Dio dio) = _MovieRetrofit;
 
   @GET("/movies")
   Future<List<MovieModel>> getMovies();
@@ -20,7 +20,7 @@ abstract class MovieRetrofit {
 class MovieRetrofitDataSource implements MovieDataSource {
   @override
   Future<List<MovieModel>> getMovies() {
-    final response = _MovieRemoteDataSource(Dio()).getMovies();
+    final response = MovieRetrofit(Dio()).getMovies();
     return response;
   }
 }
